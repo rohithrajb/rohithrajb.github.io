1f55562e93f6fe226e03b86ec9f6b9cb222ca144
Rohith Raj Bingi <rohithrajb@gmail.com>
2024-05-06 15:37:20 +0530

    Navbar implemented. Working responsively

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import {React, useState } from "react";
+import React from "react";
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
+import { Popover } from "flowbite-react";
 
 const Navbar = () => {
-   const [menuOpen, setMenuOpen] = useState(false)
-
    return (
       <nav className="flex border-b border-border text-main">
          <Link
@@ -12,15 +11,28 @@ const Navbar = () => {
             to="/">
             _rohithrajb
          </Link>
-         <ul className={ menuOpen ? 'open' : ''}>
+         <ul>
             <NavLink to="/">_hello</NavLink>
             <NavLink to="/about-me">_about-me</NavLink>
             <NavLink to="/projects">_projects</NavLink>
             <NavLink to="/contact-me">_contact-me</NavLink>
          </ul>
-         <div className="burger-menu">
-            
-         </div>
+
+         <Popover aria-labelledby="default-popover"
+            content={
+               <div className="flex flex-col bg-gray-100 px-3 w-64 text-main">
+                  <NavLink to="/">_hello</NavLink>
+                  <NavLink to="/about-me">_about-me</NavLink>
+                  <NavLink to="/projects">_projects</NavLink>
+                  <NavLink to="/contact-me">_contact-me</NavLink>
+               </div>
+            }>
+            <button>
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-3 ml-3">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+               </svg>
+            </button>
+         </Popover>
       </nav>
    );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
    content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
+      flowbite.content()
    ],
    theme: {
       extend: {
@@ -15,5 +18,7 @@ export default {
          },
       },
    },
-   plugins: [],
+   plugins: [
+      flowbite.plugin()
+   ],
 }
