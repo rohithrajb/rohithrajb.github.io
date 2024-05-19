77f77cb4e876090c62bb476c8d9536f1ff8fd932
Rohith Raj Bingi <rohithrajb@gmail.com>
2024-05-19 23:43:43 +0530

    Defined state variables for filters and projects. filterProjects() function is incomplete.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { AdvancedImage } from '@cloudinary/react'
 import { Cloudinary } from '@cloudinary/url-gen'
 import { fill } from '@cloudinary/url-gen/actions/resize'
-import { Link } from 'react-router-dom'
 
-const Project = ({ id, name, description, imageId, githubLink }) => {
+const Project = ({ project }) => {
    const cld = new Cloudinary({
       cloud: {
          cloudName: 'dbgqgm4bm',
       },
    })
 
-   const myImage = cld.image(imageId)
+   const myImage = cld.image(project.imageId)
 
    myImage.resize(fill().width(384).height(216))
 
    return (
       <div>
          <p className='text-sm'>
-            <span className='text-base text-blue-700 font-bold'>Project {id} </span>// {name}
+            <span className='text-base text-blue-700 font-bold'>Project {project.id} </span>// {project.name}
          </p>
          <div className='mt-3 bg-dark max-w-96 min-w-96 rounded-2xl'>
             <AdvancedImage cldImg={myImage} className='rounded-t-2xl' />
             <div className='py-6 px-8'>
-               <p className='mb-4'>{description}</p>
-               <Link to={githubLink} target='_blank' className='px-3 py-2 bg-[#1c2b3a] hover:bg-[#263b50] rounded-md text-white text-sm '>
+               <p className='mb-4'>{project.description}</p>
+               <Link to={project.githubLink} target='_blank' className='px-3 py-2 bg-[#1c2b3a] hover:bg-[#263b50] rounded-md text-white text-sm '>
                   view-project
                </Link>
             </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,11 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import SidebarSection from '../components/SidebarSection'
 import Project from '../components/Project'
 import './Projects.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-   faAngular,
-   faCss3,
-   faHtml5,
-   faJs,
-   faNode,
-   faNodeJs,
-   faReact,
-} from '@fortawesome/free-brands-svg-icons'
-import projectsData from '../projects.json'
+import { faAngular, faCss3, faHtml5, faJs, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import projects from '../projects.json'
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
@@ -56,37 +47,71 @@ const Projects = () => {
       },
    ]
 
+   const [selectedFilters, setSelectedFilters] = useState([])
+   const [filteredProjects, setFilteredProjects] = useState(projects)
+
+   const handleFilterClick = (clickedFilter) => {
+      if (selectedFilters.includes(clickedFilter.text)) {
+         // below line basically removes the clickedFilter from selectedFilters
+         let newFilters = selectedFilters.filter((e) => e !== clickedFilter.text)
+         setSelectedFilters(newFilters)
+      } else {
+         setSelectedFilters([...selectedFilters, clickedFilter.text])
+      }
+   }
+
+   useEffect(() => {
+      filterProjects()
+   }, [selectedFilters])
+
+   const filterProjects = () => {
+      if (selectedFilters.length > 0) {
+         // console.log(filteredProjects)
+         let tempProjects = selectedFilters.map((eachFilter) => {
+            let temp = projects.filter((project) => project.tech.includes(eachFilter))
+            console.log(temp)
+            return temp
+         })
+         // removeDuplicates(tempProjects.flat())
+         setFilteredProjects(tempProjects.flat())
+      }
+   }
+
+   // so we got a lil diverted from out goal. we were adding all the projects into the filteredProjects list which were matching atleast 1 filter. that's not cool at all. we should display projects which match all the selected filters.
+
+   // TODO: complete the removeDuplicates function
+   const removeDuplicates = (nums) => {}
+
    return (
       <div className='flex'>
          <Sidebar>
             <SidebarSection sectionName='technologies-used'>
                <div className='py-5 px-6 border-b border-border align-middle'>
                   {filters.map((filter) => (
-                     <div key={filter.id} className='mb-4'>
-                        <label className='align-middle cursor-pointer'>
+                     <div
+                        key={filter.id}
+                        className={`mb-4 cursor-pointer ${
+                           selectedFilters.includes(filter.text) ? 'filter-active' : ''
+                        }`}
+                        onClick={() => handleFilterClick(filter)}>
+                        {/* TODO: removing the radio button is only a temporary solution. solve it later */}
+                        {/* <label className='align-middle cursor-pointer'>
                            <input
                               type='checkbox'
                               id={filter.id}
                               className='checkbox rounded-sm text-sm bg-transparent border-2 mr-3 hover:shadow-input'
-                           />
-                           <FontAwesomeIcon icon={filter.icon} className='mr-2 text-xl align-middle' />
-                           {filter.text}
-                        </label>
+                           /> */}
+                        <FontAwesomeIcon icon={filter.icon} className='mr-2 text-xl align-middle' />
+                        {filter.text}
+                        {/* </label> */}
                      </div>
                   ))}
                </div>
             </SidebarSection>
          </Sidebar>
          <div className='projects-list'>
-            {projectsData.map((project) => (
-               <Project
-                  key={project.id}
-                  id={project.id}
-                  name={project.name}
-                  description={project.description}
-                  imageId={project.imageId}
-                  githubLink={project.link}
-               />
+            {filteredProjects.map((project) => (
+               <Project key={project.id} project={project} />
             ))}
          </div>
       </div>
